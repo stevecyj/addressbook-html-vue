d56d4c63587e725d862a0ad012c4fee064209d13
Steve_Tsao <steve_tsao@bb.com>
2019-12-04 18:26:01 +0800

    add pagination

--- js/all.js
+++ js/all.js
@@ -2,7 +2,25 @@ const app = new Vue({
   el: '#app',
   data: {
     text: 'hahahahah',
-    data: []
+    data: [],
+    currentPage: 0 //預設目前頁數
+  },
+  computed: {
+    filterData() {
+      const vm = this;
+      //有幾頁
+      //每頁的資料內容
+      const newData = [];
+      vm.data.forEach((item, i) => {
+        if (i % 5 === 0) {
+          newData.push([]);
+        }
+        const page = parseInt(i / 5);
+        newData[page].push(item);
+      });
+      console.log(newData);
+      return newData;
+    }
   },
   created() {
     const vm = this;
